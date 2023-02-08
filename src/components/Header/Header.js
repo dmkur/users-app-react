67861271c6d8c9a2d2cb5501c3c1544cf9ec65ca
dmkur <fgameua@gmail.com>
2023-02-08 13:33:21 +0000

    Update 2 files

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import "./header.style.css";
+
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
-      <div>
-        Header component 
+    <div className="headerWrapper">
+      <div>Header</div>
+      <div className="headerBTN">
+        <button onClick={() => navigate("/register")}>Register</button>
+        <button onClick={() => navigate("/login")}>Login</button>
       </div>
-  )
+    </div>
+  );
 };
 
-export {Header};
+export { Header };
