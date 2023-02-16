4a7cb02c576a75c6186698df4e87c4c77235644f
dmkur <fgameua@gmail.com>
2023-02-16 16:06:04 +0000

    Update 4 files

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,40 +1,38 @@
-import {useForm} from "react-hook-form";
-import {useDispatch} from "react-redux";
-import {useNavigate} from "react-router-dom";
-import {authActions} from "../../redux";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { authActions } from "../../redux";
 
 const LoginForm = () => {
-    const { register, handleSubmit, reset } = useForm();
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const { register, handleSubmit, reset } = useForm();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-
-    const submit = (userData) => {
-        console.log(userData)
-        const { error } = dispatch(authActions.login({ userData }));
-        if (!error) {
-            navigate("/cars");
-        }
-        reset();
-    };
+  const submit = (userData) => {
+    const { error } = dispatch(authActions.login({ userData }));
+    if (!error) {
+      navigate("/cars");
+    }
+    reset();
+  };
 
   return (
-      <form onSubmit={handleSubmit(submit)} style={{textAlign:'center'}}>
-          <div >
-              <label >Password: </label>
-          </div>
-          <input type={"password"} {...register("password")} />
+    <form onSubmit={handleSubmit(submit)} style={{ textAlign: "center" }}>
+      <div>
+        <label>Password: </label>
+      </div>
+      <input type={"password"} {...register("password")} />
 
-          <div>
-              <label>Email: </label>
-          </div>
-          <input type={"text"} {...register("email")} />
+      <div>
+        <label>Email: </label>
+      </div>
+      <input type={"text"} {...register("email")} />
 
-          <div style={{marginTop:'10px'}}>
-              <button type={"submit"}>Submit</button>
-          </div>
-      </form>
-  )
+      <div style={{ marginTop: "10px" }}>
+        <button type={"submit"}>Submit</button>
+      </div>
+    </form>
+  );
 };
 
-export {LoginForm};
+export { LoginForm };
