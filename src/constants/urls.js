eba2e4637d86e5f09328c09d62aee59b927ab3e7
dmkur <fgameua@gmail.com>
2023-02-17 09:09:45 +0000

    Update urls.js and auth.service.js

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,13 +1,11 @@
-const baseURL = 'https://dev-users-app.herokuapp.com/'
+const baseURL = "https://dev-users-app.herokuapp.com/";
+// const baseURL = 'https://4gxbt5-5000.preview.csb.app/'
 
 const urls = {
-    users: 'users',
-    login: 'auth/login',
-    cars: 'cars',
-    refresh: 'auth/refresh'
-}
+  users: "users",
+  login: "auth/login",
+  cars: "cars",
+  refresh: "auth/refresh",
+};
 
-export {
-    baseURL,
-    urls
-}
+export { baseURL, urls };
