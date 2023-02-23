1c407c6c320c73b82e4a4ec4a93c5f47c55af01b
dmkur <fgameua@gmail.com>
2023-02-23 14:51:04 +0000

    Update 6 files

--- src/components/Cars/Cars.js
+++ src/components/Cars/Cars.js
@@ -1,28 +1,30 @@
-import {useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {carActions} from "../../redux";
-import {Car} from "../Car/Car";
-import {CarForm} from "../CarForm/CarForm";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { carActions } from "../../redux";
+import { Car } from "../Car/Car";
+import { CarForm } from "../CarForm/CarForm";
 
 const Cars = () => {
-    const {cars, carForUpdate} = useSelector((state) => state.carReducer);
+  const { cars, carForUpdate, isLoading } = useSelector(
+    (state) => state.carReducer
+  );
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(carActions.getAll());
-    }, [carForUpdate]);
+  useEffect(() => {
+    dispatch(carActions.getAll());
+  }, [carForUpdate]);
 
-    if (!cars) {
-        return <h2>Loading...</h2>;
-    }
+  if (!isLoading) {
+    return <h2>Loading...</h2>;
+  }
 
-    return (
-        <div>
-            <CarForm/>
-            <div>{cars && cars.map((car) => <Car car={car} key={car._id}/>)}</div>
-        </div>
-    );
+  return (
+    <div>
+      <CarForm />
+      <div>{cars && cars.map((car) => <Car car={car} key={car._id} />)}</div>
+    </div>
+  );
 };
 
-export {Cars};
+export { Cars };
--- src/constants/urls.js
+++ src/constants/urls.js
@@ -3,6 +3,7 @@ const baseURL = "https://dev-users-app.herokuapp.com/";
 const urls = {
   users: "users",
   login: "auth/login",
+  logout: "auth/logout",
   cars: "cars",
   refresh: "auth/refresh",
 };
