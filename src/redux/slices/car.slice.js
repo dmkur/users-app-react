a5645a9587a76e463be51244e71546ef5a1e77ea
dmkur <fgameua@gmail.com>
2023-02-23 10:49:05 +0000

    Update car.slice.js and car.service.js

--- src/redux/slices/car.slice.js
+++ src/redux/slices/car.slice.js
@@ -1,86 +1,89 @@
-import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
-import {carService} from "../../services";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { carService } from "../../services";
 
 const initialState = {
-    cars: [],
-    error: null,
-    carForUpdate: null,
+  cars: [],
+  error: null,
+  carForUpdate: null,
 };
 
 const getAll = createAsyncThunk(
-    "carSlice/getAll",
-    async (_, {rejectWithValue}) => {
-        try {
-            const {data} = await carService.getAllCars();
+  "carSlice/getAll",
+  async (_, { rejectWithValue }) => {
+    try {
+      const { data } = await carService.getAllCars();
 
-            return data;
-        } catch (e) {
-            return rejectWithValue(e.response.data);
-        }
+      return data;
+    } catch (e) {
+      return rejectWithValue(e.response.data);
     }
+  }
 );
 
 const create = createAsyncThunk(
-    "carSlice/create",
-    async ({userId, carData}, {rejectWithValue}) => {
-        try {
-            const {data} = await carService.createCar(userId, carData);
-            return data;
-        } catch (e) {
-            return rejectWithValue(e.response.data);
-        }
+  "carSlice/create",
+  async ({ carData }, { rejectWithValue }) => {
+    try {
+      const { data } = await carService.createCar(carData);
+      return data;
+    } catch (e) {
+      return rejectWithValue(e.response.data);
     }
+  }
 );
 
 const updateCarById = createAsyncThunk(
-    "carSlice/updateCar",
-    async ({carId, carData}, {rejectWithValue}) => {
-        try {
-            await carService.updateCarById(carId, carData);
-            return carId;
-        } catch (e) {
-            return rejectWithValue(e.response.data);
-        }
+  "carSlice/updateCar",
+  async ({ carId, carData }, { rejectWithValue }) => {
+    try {
+      await carService.updateCarById(carId, carData);
+      return carId;
+    } catch (e) {
+      return rejectWithValue(e.response.data);
     }
+  }
 );
 
 const carSlice = createSlice({
-    name: "carSlice",
-    initialState,
-    reducers: {
-        setCarForUpdate: (state, action) => {
-            state.carForUpdate = action.payload;
-        },
+  name: "carSlice",
+  initialState,
+  reducers: {
+    setCarForUpdate: (state, action) => {
+      state.carForUpdate = action.payload;
     },
-    extraReducers: (builder) => {
-        builder
-            .addCase(getAll.fulfilled, (state, action) => {
-                state.cars = action.payload;
-            })
-            .addCase(updateCarById.fulfilled, (state, action) => {
-                const index = state.cars.findIndex((i) => i._id === action.payload);
-                Object.assign(index, action.payload);
-                state.carForUpdate = null;
-            })
-            .addDefaultCase((state, action) => {
-                const [type] = action.type.split("/").slice(-1);
-                type === "rejected"
-                    ? (state.error = action.payload)
-                    : (state.error = null);
-            });
-    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAll.fulfilled, (state, action) => {
+        state.cars = action.payload;
+      })
+      .addCase(create.fulfilled, (state, action) => {
+        state.cars.push(action.payload);
+      })
+      .addCase(updateCarById.fulfilled, (state, action) => {
+        const index = state.cars.findIndex((i) => i._id === action.payload);
+        Object.assign(index, action.payload);
+        state.carForUpdate = null;
+      })
+      .addDefaultCase((state, action) => {
+        const [type] = action.type.split("/").slice(-1);
+        type === "rejected"
+          ? (state.error = action.payload)
+          : (state.error = null);
+      });
+  },
 });
 
 const {
-    reducer: carReducer,
-    actions: {setCarForUpdate},
+  reducer: carReducer,
+  actions: { setCarForUpdate },
 } = carSlice;
 
 const carActions = {
-    getAll,
-    setCarForUpdate,
-    create,
-    updateCarById,
+  getAll,
+  setCarForUpdate,
+  create,
+  updateCarById,
 };
 
-export {carReducer, carActions};
+export { carReducer, carActions };
--- src/services/car.service.js
+++ src/services/car.service.js
@@ -3,8 +3,7 @@ import { urls } from "../constants/urls";
 
 const carService = {
   getAllCars: () => axiosService.get(urls.cars),
-  createCar: (userId, car) => axiosService.post(`${urls.cars}/${userId}`,car),
+  createCar: (car) => axiosService.post(`${urls.cars}`, car),
   updateCarById: (carId, car) => axiosService.put(`${urls.cars}/${carId}`, car),
-
 };
 export { carService };
